56d2c744acfd87199ea2a1018a67e0a669fcc792
hubertwro7 <hubertwro7@gmail.com>
2024-01-21 12:41:19 +0100

    refactoring of tabulators in table creating

--- Taster.SqlDatabase/dbo/Users.sql
+++ Taster.SqlDatabase/dbo/Users.sql
@@ -1,12 +1,11 @@
 CREATE TABLE [dbo].[Users] (
-	[Id]				INT			IDENTITY(1, 1) NOT NULL,
-	[Email]				NVARCHAR (100)	NOT NULL,
-	[Username]			NVARCHAR (100)	NOT NULL,
+	[Id]	INT	IDENTITY(1, 1) NOT NULL,
+	[Email]	NVARCHAR (100)	NOT NULL,
+	[Username]	NVARCHAR (100)	NOT NULL,
 	[HashedPassword]	NVARCHAR (200)	NOT NULL,
-	[RegisterDate]      DATETIMEOFFSET	NOT NULL,
+	[RegisterDate]	DATETIMEOFFSET	NOT NULL,
 	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-
 
 GO
 
